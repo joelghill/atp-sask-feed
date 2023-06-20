861e22667fc3f8f6b1b3eb1d6fbc96fce1a60177
Joel <joelghill@users.noreply.github.com>
2023-06-20 00:20:45 -0600

    Update 1686952398256-init.ts
    Fixing auto increment id

--- src/migrations/1686952398256-init.ts
+++ src/migrations/1686952398256-init.ts
@@ -6,7 +6,7 @@ export class Init1686952398256 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "post" ("uri" varchar PRIMARY KEY NOT NULL, "cid" varchar NOT NULL, "author" varchar NOT NULL, "replyParent" varchar, "replyRoot" varchar, "indexedAt" timestamp NOT NULL)`);
         await queryRunner.query(`CREATE TABLE "sub_state" ("service" varchar PRIMARY KEY NOT NULL, "cursor" integer NOT NULL)`);
-        await queryRunner.query(`CREATE TABLE "subscriber" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "did" varchar NOT NULL, "indexedAt" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, "lastUpdated" timestamp NOT NULL DEFAULT (), "expiresAt" date)`);
+        await queryRunner.query(`CREATE TABLE "subscriber" ("id" SERIAL PRIMARY KEY, "did" varchar NOT NULL, "indexedAt" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, "lastUpdated" timestamp NOT NULL DEFAULT (), "expiresAt" date)`);
         await queryRunner.query(`CREATE TABLE "session" ("id" varchar PRIMARY KEY NOT NULL, "expiresAt" integer NOT NULL, "data" varchar NOT NULL)`);
     }
 
